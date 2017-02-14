5135750d1e77a4624b16843d2e45d53afa3f0cee
MRbcgd <quittypig@gmail.com>
2017-02-14 17:41:42 +0900

    좌석예약기능 추가, socket.io으로 연동

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http=require('http')
 var express=require('express')
 var socketio=require('socket.io')
 
-var seats=[
+var seats=[//0 : 빈공간, 1 : 예약가능, 2 : 예약불가능
   [1,1,0,1,1,0,0,0,0,1,1,0,1,1],
   [1,1,0,1,1,1,1,1,1,1,1,0,1,1],
   [1,1,0,1,1,1,1,1,1,1,1,0,1,1],
@@ -19,25 +19,25 @@ var app=express()
 var server=http.createServer(app)
 
 app.get('/',function(req,res,next){
-  fs.readFile('HTMLPage.html',function(erorr,data){
+  fs.readFile('HTMLPage.html',function(error,data){
     if(error){
       return console.log(error);
     }
 
-    res.send(data.toString())
+    res.send(data.toString())//JSON 타입으로 전송
   })
 })
 app.get('/seats',function(req,res,next){
-  console.log(seats+'send');
   res.send(seats)
 })
 server.listen(52273,function(){
   console.log('Server running at http://127.0.0.1:52273');
 })
+
 var io=socketio.listen(server)
 io.sockets.on('connection',function(socket){
   socket.on('reserve',function(data){
-    seats[data.y][data.x]=2
+    seats[data.y][data.x]=2//server의 seat data 수정
     io.sockets.emit('reserve',data)
   })
 })
